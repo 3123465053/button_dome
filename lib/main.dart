8fc69771749dbe46ee52f2a731671560c71d3cb2
3123465053 <3123465053qq.com>
2023-06-12 20:47:13 +0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,61 @@
+import 'dart:convert';
+
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+    Map map={};
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              ElevatedButton(
+                  style: ButtonStyle(
+                    shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10))
+                    ))
+                  ),
+                  onPressed: ()async{
+                await getData();
+                setState((){});
+              }, child: Text("测试API")),
+              Container(
+                width: 300,
+                height: 400,
+                color: Colors.blue.shade100,
+                child: ListView.builder(
+                    itemCount: map.isEmpty?0:map["channels"].length,
+                    itemBuilder: (BuildContext context,index){
+                  return Padding(padding: EdgeInsets.only(left: 10,top: 10),
+                    child: Text("${map["channels"][index]["name"]}"),
+                  );
+                }),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Future<void> getData() async{
+    Dio dio=Dio();
+    String url="https://www.douban.com/j/app/radio/channels";
+    var res=await dio.get(url);
+      map= res.data;
+  }
+}
